0e6b9e3815789584d5fecde5bccef71e97eaebca
Taichi Kobayashi <taichikobayashi@TaichinoMac-mini.local>
2024-11-19 23:17:48 +0900

    - parser.go の parseStatement に RETURN ケースを追加     - parseReturnStatement 関数を実装し、return 文の構文解析を追加 - テストが成功することを確認

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -52,6 +52,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -75,6 +77,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
 	return stmt
 }
 
