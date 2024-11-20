d3bfc92018fc929579d08806a41ff4223e53033b
Taichi Kobayashi <taichikobayashi@TaichinoMac-mini.local>
2024-11-20 14:33:47 +0900

    - 基本的な式の解析機能を実装し、前置構文の解析に対応     - 前置構文解析用の関数型 'prefixParseFn' とマップ 'prefixParseFns' を追加。     - 式解析のために 'parseExpression' と 'parseIdentifier' を実装。     - 'parseStatement' に式文の解析を追加。     - トークンタイプを簡潔に判定するためのヘルパー関数 'curTokenIs' を導入。     - 優先順位の定数を追加し、初期的な優先順位処理を実装。
        これらの変更により、式の解析基盤を構築し、識別子を基本的な式として解析できるようになりました。
    
    - すべての関連テストが通過したことを確認済み。

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -8,8 +8,19 @@ import (
 	"github.com/taichi-kobaya/go-monkey-interpreter/monkey/token"
 )
 
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > または <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X または　!X
+	CALL        // myFunction(X)
+)
+
 type (
-	prefixParseFn func()                              // 前置構文解析関数
+	prefixParseFn func() ast.Expression               // 前置構文解析関数
 	infixParseFn  func(ast.Expression) ast.Expression // 中置構文解析関数
 )
 
@@ -29,6 +40,9 @@ func New(l *lexer.Lexer) *Parser {
 		l:      l,
 		errors: []string{},
 	}
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
 	p.nextToken()
@@ -39,6 +53,10 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -63,7 +81,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -100,8 +118,30 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
-func (p *Parser) curTokenIs(t token.TokenType) bool {
-	return p.curToken.Type == t
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
