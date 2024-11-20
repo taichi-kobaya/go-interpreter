770c941d333334e3c47ff3c57e1c7f31c178d533
Taichi Kobayashi <taichikobayashi@TaichinoMac-mini.local>
2024-11-21 06:56:00 +0900

    - 整数リテラルの構文解析を実装し、テストが通過     - 'parseIntegerLiteral' 関数を追加し、整数リテラルの解析機能を実装。     - 'New()' 内で 'token.INT' に対応する前置構文解析関数として登録。     - ASTに 'IntegerLiteral' ノードを追加。         - 'Token', 'Value' プロパティを含む構造体として定義。         - 'expressionNode', 'TokenLiteral', 'String' メソッドを実装。     - 構文解析のテストケース 'TestIntegerLiteralExpression' を追加し、整数リテラルの正しい解析を確認。 - 関連するすべてのテストが通過したことを確認済み。

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/taichi-kobaya/go-monkey-interpreter/monkey/ast"
 	"github.com/taichi-kobaya/go-monkey-interpreter/monkey/lexer"
@@ -43,6 +44,8 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
 	p.nextToken()
@@ -142,6 +145,20 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
 }
 
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
