2d573118f35e32234ffb17e991b5cd14f0023a1b
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-11-24 08:30:25 +0900

    ブール値の評価をリファクタリング
    - evaluator内でブール値の評価をnativeBoolToBooleanObject関数を用いてリファクタリング
    - TRUE, FALSE, NULLの定数を追加し、コードの可読性を向上
    - ブール値を返す処理を簡潔にし、パフォーマンス向上

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/taichi-kobaya/go-monkey-interpreter/monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -18,8 +24,9 @@ func Eval(node ast.Node) object.Object {
 	// 式
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -33,4 +40,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
