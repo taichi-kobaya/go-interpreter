53f3fbf9379b361dfbed1ba4abda6cf9d6e63bc5
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-11-23 15:41:07 +0900

    関数リテラルの解析機能を追加 - FunctionLiteral構造体をastに追加 - 関数リテラルとそのパラメータを解析するparseFunctionLiteralとparseFunctionParametersを実装 - 関数リテラルの解析を検証するテストを追加 - テストがすべて成功することを確認

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/taichi-kobaya/go-monkey-interpreter/monkey/token"
 )
@@ -207,6 +208,31 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
