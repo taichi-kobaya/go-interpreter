1741bdbc0940fb7e50b3c203f17e21d06ca82370
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-11-26 06:15:28 +0900

    配列添字の構文解析機能を追加しテストを実装
    - ast.IndexExpression構造体とその関連メソッドを追加
    - parserに配列添字の構文解析機能を追加
    - インデックス式に対応するテストケースをTestOperatorPrecedenceParsingに追加
    - 配列添字の構文解析を確認するためのTestParsingIndexExpressions関数を実装
    - すべての関連テストが成功することを確認

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -16,8 +16,9 @@ const (
 	LESSGREATER // > または <
 	SUM         // +
 	PRODUCT     // *
-	PREFIX      // -X または　!X
+	PREFIX      // -X または !X
 	CALL        // myFunction(X)
+	INDEX       // array[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -30,6 +31,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type (
@@ -77,6 +79,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
@@ -409,6 +412,19 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 	array.Elements = p.parseExpressionList(token.RBRACKET)
 
 	return array
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
+	return exp
 }
 
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
