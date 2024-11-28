25bb1ec94e0d83cf26a2ab76160665d71f6f7d02
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-11-28 15:07:47 +0900

    Modify関数に中置式と前置式の対応を追加
    - modify.goに中置式と前置式のノードを処理するケースを追加
    - modify_test.goに中置式および前置式のテストケースを追加
    - テストがすべて成功することを確認

--- monkey/ast/modify_test.go
+++ monkey/ast/modify_test.go
@@ -43,6 +43,18 @@ func TestModify(t *testing.T) {
 				},
 			},
 		},
+		{
+			&InfixExpression{Left: one(), Operator: "+", Right: two()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&InfixExpression{Left: two(), Operator: "+", Right: one()},
+			&InfixExpression{Left: two(), Operator: "+", Right: two()},
+		},
+		{
+			&PrefixExpression{Operator: "-", Right: one()},
+			&PrefixExpression{Operator: "-", Right: two()},
+		},
 	}
 
 	for _, tt := range tests {
